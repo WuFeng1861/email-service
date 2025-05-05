7e519637d6b89f2628ec0559d0d2106965bbced1
WuFeng1861 <1379459026@qq.com>
2025-05-06 01:31:34 +0800

    3
    3

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true }));
     app.setGlobalPrefix('api');
+    app.enableCors();
     const port = process.env.PORT || 3000;
     await app.listen(port);
     console.log(`Application is running on: http://localhost:${port}`);
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
+  // cors
+  app.enableCors();
   
   const port = process.env.PORT || 3000;
   await app.listen(port);
